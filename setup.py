9035c9b7b2b9e18f69d0e4b5bd52ce8b7bb4ade0
Anh Nguyen <ntngocanh19@gmail.com>
2022-06-25 01:50:55 +0800

    upp

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     name="executor",
     version="0.0.1",
     install_requires=REQUIRED_PACKAGES,
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages("."),
     # include_package_data=True,
     # package_data={"src": ["raw_schema/schema.pbtxt"]},
 )
