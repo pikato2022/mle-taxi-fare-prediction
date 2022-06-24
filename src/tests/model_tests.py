1cb7231f588a1023adb83338e82c594ab6934bbb
Anh Nguyen <ntngocanh19@gmail.com>
2022-06-24 17:42:30 +0800

    upp

--- src/tests/model_tests.py
+++ src/tests/model_tests.py
@@ -19,11 +19,6 @@ import tensorflow as tf
 
 from src.pipeline import trainer_tune
 
-root = logging.getLogger()
-root.setLevel(logging.INFO)
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
-root.addHandler(handler)
 EXPECTED_HYPERPARAMS_KEYS = [
     "learning_rate",
     "n_layers",
@@ -34,29 +29,3 @@ EXPECTED_HYPERPARAMS_KEYS = [
 def test_hyperparams_defaults():
     hyperparams = trainer_tune._get_hyperparameters()
     assert set(hyperparams.keys()) == set(EXPECTED_HYPERPARAMS_KEYS)
-
-def test_create_binary_classifier():
-    pass
-    hyperparams = trainer_tune._get_hyperparameters()
-
-    # model_inputs = {
-    #     "dropoff_grid_xf": tf.convert_to_tensor([0, 0, 0]),
-    #     "euclidean_xf": tf.convert_to_tensor([-0.9066112, -0.9066112, -0.9066112]),
-    #     "loc_cross_xf": tf.convert_to_tensor([0, 0, 0]),
-    #     "payment_type_xf": tf.convert_to_tensor([1, 0, 0]),
-    #     "pickup_grid_xf": tf.convert_to_tensor([0, 0, 0]),
-    #     "trip_day_of_week_xf": tf.convert_to_tensor([5, 4, 4]),
-    #     "trip_day_xf": tf.convert_to_tensor([26, 24, 1]),
-    #     "trip_hour_xf": tf.convert_to_tensor([0, 4, 2]),
-    #     "trip_miles_xf": tf.convert_to_tensor([5.9717827, -0.7121308, -0.7601589]),
-    #     "trip_month_xf": tf.convert_to_tensor([4, 3, 4]),
-    #     "trip_seconds_xf": tf.convert_to_tensor([4.9029775, -0.34146854, -0.34479955]),
-    # }
-    #
-    # feature_vocab_sizes = {
-    #     feature_name: 100 for feature_name in features.categorical_feature_names()
-    # }
-    # models = trainer_tune._make_keras_model(hyperparams)
-    # model_outputs = models(model_inputs)  # .numpy()
-    # assert model_outputs.shape == (3, 1)
-    # assert model_outputs.dtype == "float32"
